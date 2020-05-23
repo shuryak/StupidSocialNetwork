15465ba25b7de11e1963bbbda765d97e83228549
Alexander Konovalov <shuryak@protonmail.com>
2020-05-23 12:21:22 +0300

    Refactoring of Router.php

--- application/core/Router.php
+++ application/core/Router.php
@@ -4,102 +4,69 @@ namespace application\core;
 use application\core\Security;
 
 class Router {
+    private static $routes = []; // Allowed Routes
 
-    protected static $routes = [];
-    protected static $routesArray = [];
-    protected static $params = [];
+    // This method gets allowed routes from config files
+    private static function getAllowedRoutes() {
+        // Allowed routes are routes from the config files
+        $allowedRoutes = array_merge(require 'application/config/routes.php', require 'application/config/api.php');
 
-    public static function addRoutes() {
-        $allowedRoutes = require 'application/config/routes.php';
+        $regexAllowedRoutes = [];
+
         foreach($allowedRoutes as $route => $routeArray) {
-            self::add($route, $routeArray);
+            $regexAllowedRoutes['#^'.$route.'$#'] = $routeArray; // Preparing for regex
         }
+
+        return $regexAllowedRoutes;
     }
 
-    public static function addApi() {
-        $allowedRoutes = require 'application/config/api.php';
-        foreach($allowedRoutes as $route => $routeArray) {
-            self::add($route, $routeArray);
-        }
+    // This method checks route string for api route
+    private static function isApi($route) {
+        return preg_match('/api\/(.*)\.(.*)/', $route); // Incomplete regex (!)
     }
 
-    public static function isApi($route) {
-        return preg_match('/api\/(.*)\.(.*)/', $route); // Incomplete regex
-    }
+    // This method finds matches request url with allowed routes
+    private static function matchRoute($isApi = false) {
+        if($isApi) $url = str_replace('/api/', '', $_SERVER['REQUEST_URI']);
+        else $url = trim($_SERVER['REQUEST_URI'], '/');
 
-    public static function add($route, $routeArray) {
-        $route = '#^'.$route.'$#';
-        self::$routes[$route] = $routeArray;
-    }
-
-    public static function matchRoute() {
-        $url = trim($_SERVER['REQUEST_URI'], '/');
-        foreach(self::$routes as $route => $routeArray) {
-            if(preg_match($route, parse_url($url, PHP_URL_PATH))) {
-                self::$routesArray = $routeArray;
-                self::$params = parse_url($url, PHP_URL_QUERY) != NULL ? parse_url($url, PHP_URL_QUERY) : '';
-                return true;
+        foreach(self::$routes as $route => $routeArray) { // Enumeration of allowed routes
+            if(preg_match($route, parse_url($url, PHP_URL_PATH))) { // Match check
+                return [
+                    'routes_array' => $routeArray,
+                    'params' => parse_url($url, PHP_URL_QUERY) != NULL ? $_GET : ''
+                ];
             }
         }
-        return false;
-    }
 
-    public static function matchApi() {
-        // $url = trim($_SERVER['REQUEST_URI'], '/api');
-        $url = str_replace('/api/', '', $_SERVER['REQUEST_URI']);
-        // var_dump($url);
-        list($controller, $method) = explode(".", $url);
-        foreach(self::$routes as $route => $routeArray) {
-            if(preg_match($route, parse_url($url, PHP_URL_PATH))) {
-                self::$routesArray = $routeArray;
-                self::$params = parse_url($url, PHP_URL_QUERY) != NULL ? $_GET : '';
-                return true;
-            }
-        }
         return false;
     }
 
     public static function run() {
+        self::$routes = self::getAllowedRoutes();
+
+        /*
+        Controller actions have the following format: <Controller_Name>::<Action_Name>Action;
+        Controller api methods have the following format: <Controller_Name>::<Method_Name>Api.
+        */
         if(self::isApi(trim($_SERVER['REQUEST_URI'], '/'))) {
-            if(!Security::checkForApiLimit()) {
-                exit(View::errorCode(429));
-            }
-            self::addApi();
-            if(self::matchApi()) {
-                $path = 'application\controllers\\'.ucfirst(self::$routesArray['controller']).'Controller';
-                if(class_exists($path)) {
-                    $method = self::$routesArray['method'].'Api';
-                    if(method_exists($path, $method)) {
-                        $path::load(self::$routesArray);
-                        $path::$method();
-                    } else {
-                        View::errorCode(404);
-                    }
-                }
-                else {
-                    View::errorCode(404);
-                }
-            } else {
-                View::errorCode(404);
-            }
+            if(!Security::checkForApiLimit()) exit(View::errorCode(429)); // If the request limit is exceeded, throw an http error 429
+
+            $matchResult = self::matchRoute(true);
+            $method = $matchResult['routes_array']['method'].'Api';
         } else {
-            self::addRoutes();
-            if(self::matchRoute()) {
-                $path = 'application\controllers\\'.ucfirst(self::$routesArray['controller']).'Controller';
-                if(class_exists($path)) {
-                    $action = self::$routesArray['action'].'Action';
-                    if(method_exists($path, $action)) {
-                        $path::load(self::$routesArray);
-                        $path::$action();
-                    } else {
-                        View::errorCode(404);
-                    }
-                } else {
-                    View::errorCode(404);
-                }
-            } else {
-                View::errorCode(404);
-            }
+            $matchResult = self::matchRoute();
+            $method = $matchResult['routes_array']['action'].'Action';
+        }
+
+        $path = 'application\controllers\\'.ucfirst($matchResult['routes_array']['controller']).'Controller'; // Path of controller
+
+        if($matchResult && class_exists($path) && method_exists($path, $method)) {
+            // All controllers extends core/Controller.php which contains method load()
+            $path::load($matchResult['routes_array']);
+            $path::$method();
+        } else {
+            View::errorCode(404);
         }
     }
 }
